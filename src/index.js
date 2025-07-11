bc6b45b8030c5d82130b9f19ed0aaf4a972d4c4e
Mr. Bhushan Gulabrao Patil. <bhushan.patil@globalion.in>
2025-07-11 16:53:29 +0530

    update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import 'react-toastify/dist/ReactToastify.css';
+import "react-toastify/dist/ReactToastify.css";
 import "./index.css";
 import App from "./App";
 // import * as serviceWorker from "./serviceWorker";
@@ -9,37 +9,36 @@ import { Provider } from "react-redux";
 import rootReducer from "./reducers";
 import { loadState, saveState } from "./localStorage";
 import { ToastContainer } from "react-toastify";
-import './i18n'; // Import i18n configuration
-import { I18nextProvider } from 'react-i18next';
-import i18n from './i18n';
-import store from './store/storage'; 
+import "./i18n"; // Import i18n configuration
+import { I18nextProvider } from "react-i18next";
+import i18n from "./i18n";
+import store from "./store/storage";
 const persistedState = loadState();
-
+//
 // const store = createStore(rootReducer, persistedState);
 
 // store.subscribe(() => {
 //   saveState(store);
 // });
 
-
-
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
     <ToastContainer
-          position="top-right"
-          autoClose={5000}
-          hideProgressBar
-          newestOnTop
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-        />
-        <I18nextProvider i18n={i18n}> <App /></I18nextProvider>
-   
+      position="top-right"
+      autoClose={5000}
+      hideProgressBar
+      newestOnTop
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+    />
+    <I18nextProvider i18n={i18n}>
+      {" "}
+      <App />
+    </I18nextProvider>
   </Provider>
 );
 
